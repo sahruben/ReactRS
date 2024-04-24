6a03ee64fc5046dcf71bdce373ea416a17ff64df
Ruben Sahakyan <sahakyanruben1981@gmail.com>
2024-04-24 13:45:41 +0200

    Lesson04

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import "./App.css";
 // import Lesson02 from "./lessons/Lessons02/Lessons02";
 // import Homework02 from "./homeworks/Homework02/Homework02";
-import Lesson03 from "./lessons/Lesson03/Lesson03";
+// import Consultation01 from "./consultations/Consultation01/Consultation01";
+// import Lesson03 from "./lessons/Lesson03/Lesson03";
+// import Homerwork03 from "./homeworks/Homework03/Homework03";
+import Lesson04 from "./lessons/Lesson04/Lesson04";
 
 function App() {
   return (
     <div className="App">
       {/* <Lesson02 /> */}
       {/* <Homework02 /> */}
-      <Lesson03 />
+      {/* <Lesson03 /> */}
+      {/* <Consultation01 /> */}
+      {/* <Homerwork03 /> */}
+      <Lesson04 />
     </div>
   );
 }
 
 export default App;
+
