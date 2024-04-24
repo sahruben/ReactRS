7d26300d09029f5f2549e0f61027088e19363f4b
Ruben Sahakyan <sahakyanruben1981@gmail.com>
2024-04-24 14:08:42 +0200

    Consultation02

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import "./App.css";
+
+// Лекции
 // import Lesson02 from "./lessons/Lessons02/Lessons02";
-// import Homework02 from "./homeworks/Homework02/Homework02";
-// import Consultation01 from "./consultations/Consultation01/Consultation01";
 // import Lesson03 from "./lessons/Lesson03/Lesson03";
+// import Lesson04 from "./lessons/Lesson04/Lesson04";
+// import Lesson05 from "./lessons/Lesson05/Lesson05";
+
+// Домашние работы
+// import Homework02 from "./homeworks/Homework02/Homework02";
 // import Homerwork03 from "./homeworks/Homework03/Homework03";
-import Lesson04 from "./lessons/Lesson04/Lesson04";
+// import Homerwork04 from "./homeworks/Homework04/Homework04";
+
+// Консультации
+// import Consultation01 from "./consultations/Consultation01/Consultation01";
+import Consultation02 from "./consultations/Consultation02/Consultation02";
 
 function App() {
   return (
@@ -14,10 +23,12 @@ function App() {
       {/* <Lesson03 /> */}
       {/* <Consultation01 /> */}
       {/* <Homerwork03 /> */}
-      <Lesson04 />
+      {/* <Lesson04 /> */}
+      {/* <Homerwork04 /> */}
+      {/* <Lesson05 /> */}
+      <Consultation02 />
     </div>
   );
 }
 
 export default App;
-
